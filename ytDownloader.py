038c6df14969eda27ca701ca8f4224e2cd3346f2
Mavdavis <davidmarvelous@seamlesshr.com>
2024-05-01 22:08:29 +0100

    python youtube downloader

---
+++ ytDownloader.py
@@ -0,0 +1,22 @@
+from pytube import YouTube
+
+video_url = input("Enter the YouTube video URL: ")
+
+yt = YouTube(video_url)
+
+print("Title: ", yt.title)
+
+streams = yt.streams.all()
+
+print("Available qualities:")
+for stream in streams:
+    print(stream.resolution)
+
+quality = input("Enter the quality of video you want to download: ")
+
+available_streams = [stream for stream in streams if stream.resolution == quality]
+if available_streams:
+    yd = available_streams[0]
+    yd.download('/Users/davidmarvelous/Downloads')
+else:
+    print("Sorry, the chosen quality is not available for this video.")
