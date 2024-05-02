0600293142dce1d57d9abe6701b1c96f86242485
Mavdavis <davidmarvelous@seamlesshr.com>
2024-05-02 19:54:02 +0100

    learnt a line of code

--- ytDownloader.py
+++ ytDownloader.py
@@ -13,7 +13,7 @@ for stream in streams:
     print(stream.resolution)
 
 quality = input("Enter the quality of video you want to download: ")
-
+# stream for stream in streams loops through the streams gotten from the youtube url and we then check if the user input exist in that stream
 available_streams = [stream for stream in streams if stream.resolution == quality]
 if available_streams:
     yd = available_streams[0]
